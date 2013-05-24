3ce4616996f466f04d56ff168617634e8e20c829
Svenn-Arne Dragly <s@dragly.com>
2013-05-24 19:12:27 +0200

    Reduced the number of exponentials and divisions.

--- src/force/vashishtatwoparticleforce.cpp
+++ src/force/vashishtatwoparticleforce.cpp
@@ -35,20 +35,31 @@ void VashishtaTwoParticleForce::calculateAndApplyForce(Atom *atom1, Atom *atom2,
     double etaij = eta[combo];
     double Zi = atom1->type().effectiveCharge();
     double Zj = atom2->type().effectiveCharge();
+    double Zi2 = Zi * Zi;
+    double Zj2 = Zj * Zj;
     double alphai = atom1->type().electronicPolarizability();
     double alphaj = atom2->type().electronicPolarizability();
     double r4s = 4.43;
     double r2 = dot(rVec, rVec);
     double r = sqrt(r2);
-    double r4 = r2 * r2;
+//    double r3 = r2 * r;
+//    double r4 = r2 * r2;
 //    double r5 = r2 * r2 * r;
 //    double r6 = r5 * r;
+    double invr = 1 / r;
+    double invr2 = invr * invr;
+    double invr3 = invr2 * invr;
+    double invr4 = invr2 * invr2;
+    double invr5 = invr4 * invr;
+    double invr6 = invr3 * invr3;
+    double invr4s = 1 / r4s;
+    double expR4OverR4s = exp(-r/r4s);
 
-    Vector3 force = -rVec*(-Hij*etaij*pow(r, -etaij)/pow(r, 2) - Zi*Zj/pow(r, 3) + (0.5*pow(Zi, 2)*alphaj + 0.5*pow(Zj, 2)*alphai)*exp(-r/r4s)/(pow(r, 5)*r4s) + 4*(0.5*pow(Zi, 2)*alphaj + 0.5*pow(Zj, 2)*alphai)*exp(-r/r4s)/pow(r, 6));
+    Vector3 force = -rVec*(-Hij*etaij*pow(r, -etaij) * invr2 - Zi*Zj * invr3 + (0.5*Zi2*alphaj + 0.5*Zj2*alphai)*expR4OverR4s*invr5*invr4s + 4*(0.5*Zi2*alphaj + 0.5*Zj2*alphai)*expR4OverR4s * invr6);
 
-    double potential = Hij / pow(r, etaij)
-            + (Zi * Zj) / r
-            - (0.5 * (alphai * Zj * Zj + alphaj * Zi * Zi) / r4) * exp(-r / r4s);
+    double potential = Hij * pow(invr, etaij)
+            + (Zi * Zj) * invr
+            - (0.5 * (alphai * Zj * Zj + alphaj * Zi * Zi) * invr4) * expR4OverR4s;
 
     if(m_isNewtonsThirdLawEnabled) {
         atom2->addForce(force);
