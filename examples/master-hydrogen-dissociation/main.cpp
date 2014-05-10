2d15eb366ba067e351ba0c1efe7db5fa8df6e7a7
Svenn-Arne Dragly <s@dragly.com>
2014-05-10 19:02:06 +0200

    Catching correct exception for type errors

--- examples/master-hydrogen-dissociation/main.cpp
+++ examples/master-hydrogen-dissociation/main.cpp
@@ -72,7 +72,7 @@ int main(int argc, char* argv[])
         // Read optional params or catch exception
         rootNode["cell_cutoff"] >> cellCutoff;
         rootNode["cutoff_radius"] >> cutoffRadius;
-    } catch(YAML::ParserException& ) {
+    } catch(YAML::TypedKeyNotFound& ) {
     }
 
     double numberDensity = density / (mp/me);
